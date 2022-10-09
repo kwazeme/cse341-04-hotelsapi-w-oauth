8876f17361ccc2f32b70676224f925b21074fc5b
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-10-09 21:57:47 +0200

    update L08

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,23 @@ const mongodb = require('./db/connect');
 require('dotenv').config();
 
 const port = process.env.PORT || 8080;
-const {auth, requiresAuth } = require('express-openid-connect');
+// const {auth, requiresAuth } = require('express-openid-connect');
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: process.env.baseURL,
-  clientID: process.env.clientID,
-  issuerBaseURL: process.env.issuerBaseURL,
-  secret: process.env.secret
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   baseURL: process.env.baseURL,
+//   clientID: process.env.clientID,
+//   issuerBaseURL: process.env.issuerBaseURL,
+//   secret: process.env.secret
+// };
 
 const cors = require('cors');
 
 // install express
 const app = express();
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+// app.use(auth(config));
   
 
 // initialize routes
@@ -35,14 +35,14 @@ app
     console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
   });
 
-  // req.isAuthenticated is provided from the auth router
-  app.get('/', (req,res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-  });
+  // // req.isAuthenticated is provided from the auth router
+  // app.get('/', (req,res) => {
+  //   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  // });
 // get the user profile
-  app.get('/profile', requiresAuth(), (req, res) => {
-    res.send(JSON.stringify(req.oidc.user));
-  });
+  // app.get('/profile', requiresAuth(), (req, res) => {
+  //   res.send(JSON.stringify(req.oidc.user));
+  // });
   
 mongodb.initDb((err) => {
   if (err) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const {requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+const { auth, requiresAuth } = require('express-openid-connect');
+const app = express();
+  const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: process.env.baseURL,
+    clientID: process.env.clientID,
+    issuerBaseURL: process.env.issuerBaseURL,
+    secret: process.env.secret
+  };
+
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+  // // req.isAuthenticated is provided from the auth router
+router.get('/', (req,res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
 
 router.use('/ss_hotels', require('./hotels'));
 router.use('/', require('./swagger'));
@@ -12,6 +30,8 @@ router.use('/reservations', requiresAuth, require('./hotels'), (req, res) =>
 router.use('/users', requiresAuth, require('./hotels'), (req, res) =>
     res.send(`Welcome ${req.oidc.user.sub}, Please make your reservation here.`)
     );
-
+router.get('/profile', requiresAuth(), (req, res) => {
+        res.send(JSON.stringify(req.oidc.user));
+      });
 
 module.exports = router;
